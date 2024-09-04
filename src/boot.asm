d4268c2a6c2c024cf2f42db68e597fe53d0a6096
Extraskilled56 <zjones.2012@icloud.com>
2024-09-03 20:16:56 -0500

    final commit for the day

--- src/boot.asm
+++ src/boot.asm
@@ -11,11 +11,12 @@ section .text
 
 global start
 extern boot 				;this is defined in the c file
-
+extern main
 start:
 	cli 				;block interrupts
 	mov esp, stack_space		;set stack pointer
 	call boot
+        call main
 	hlt 				;halt the CPU
 
 section .bss
---
+++ src/include/screen.h
@@ -0,0 +1,34 @@
+
+
+/* video memory begins at address 0xb8000 */
+char *vidptr = (char*)0xb8000;
+unsigned int i = 0;
+unsigned int j = 0;
+unsigned int screensize;
+
+void clear_screen() {
+    /* this loops clears the screen
+	* there are 25 lines each of 80 columns; each element takes 2 bytes */
+	screensize = 80 * 25 * 2;
+	while (j < screensize) {
+		/* blank character */
+		vidptr[j] = ' ';
+		/* attribute-byte */
+		vidptr[j+1] = 0x07;
+		j = j + 2;
+	}
+}
+
+void print_hello_world() {
+    const char *str = "Hello, World!";
+	j = 0;
+	/* this loop writes the string to video memory */
+	while (str[j] != '\0') {
+		/* the character's ascii */
+		vidptr[i] = str[j];
+		/* attribute-byte: give character black bg and light grey fg */
+		vidptr[i+1] = 0x07;
+		++j;
+		i = i + 2;
+	}
+}
