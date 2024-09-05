597c74dba5358b0117ce48c52c96b194a76249e6
Extraskilled56 <zjones.2012@icloud.com>
2024-09-04 20:15:16 -0500

    made build iso script and update others

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -4,4 +4,4 @@ nasm -f elf32 boot.asm -o tmp/kasm.o
 
 gcc -m32 -c boot.c -o tmp/kc.o
 
-ld -m elf_i386 -T link.ld -o ec-kernel.iso tmp/kasm.o tmp/kc.o
+ld -m elf_i386 -T link.ld -o ec-kernel tmp/kasm.o tmp/kc.o
--- src/scripts/vm-test.sh
+++ src/scripts/vm-test.sh
@@ -2,6 +2,6 @@ cd ~/Echo-kernel/src
 
 echo starting vm
 
-qemu-system-i386 -kernel ec-kernel.iso
+qemu-system-i386 -kernel ec-kernel
 
 echo end
