3ab2ed0a2b1ef24763b4dbe9a599334efce31d5d
Extraskilled56 <zjones.2012@icloud.com>
2024-09-05 19:55:23 -0500

    some more updare

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -2,8 +2,8 @@ set -e
 
 cd ~/Echo-kernel/src
 
-nasm -f elf32 boot/boot.asm -o tmp/bootkc.o
+nasm -f elf32 boot.asm -o tmp/bootkc.o
 
-gcc -m32 -c boot/boot.c -o tmp/kc.o
+gcc -m32 -c boot.c -o tmp/kc.o
 
 ld -m elf_i386 -T link.ld -o ec-kernel tmp/bootkc.o tmp/kc.o
