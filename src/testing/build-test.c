5c5746b29a7ff4e2826b0fe033717272b001795e
Extraskilled56 <zjones.2012@icloud.com>
2024-09-08 09:27:48 -0500

    fix build and test program

--- src/testing/build-test.c
+++ src/testing/build-test.c
@@ -3,17 +3,8 @@
 
 
 int main() {
-    if (system("nasm -f elf32 kernel.asm -o tmp/kasm.o") == 1) {
-        printf("Assembly failed.\n");
+    if (system("bash scripts/build.sh") == 1) {
+        printf("Build failed.\n");
         return 1;
     }
-    if (system("gcc -m32 -c kernel.c -o tmp/kc.o") == 1) {
-        printf("Compilation failed.\n");
-        return 1;
-    } 
-    if (system("ld -m elf_i386 -T link.ld -o kernel tmp/kasm.o tmp/kc.o") == 1) {
-        printf("Linking failed.\n");
-        return 1;
-    }
-    printf("Build and test successful.\n");
 }
